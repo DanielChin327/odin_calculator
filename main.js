066c0555f8730694579a77292421fb01348facbb
Daniel Chin <DanChin327@gmail.com>
2024-05-19 13:25:29 -0600

    added a note for large numbers on screen.

--- main.js
+++ main.js
@@ -35,9 +35,16 @@ document.addEventListener("DOMContentLoaded", function(){
   })
 
   equal.addEventListener("click", function (){
-    calculate()
-    previousScreen.textContent = "";
-    currentScreen.textContent = previousValue;
+    if(currentValue != '' && previousValue != ''){
+      calculate()
+      previousScreen.textContent = "";
+      if(previousValue.length <=8){
+        currentScreen.textContent = previousValue;
+      }
+      else {
+        currentScreen.textContent = previousValue.slice(0,5) + "...";
+      }
+    }
   })
 })
 
